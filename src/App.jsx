2e08dedd8ac920db322680af8f908a8bdafab596
maria.pilar <maria_aponte@estudante.sesisenai.org.br>
2025-05-23 21:59:06 -0300

    primeiro exercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function adivinhar(){
+    let aleatorio = Math.ceil(Math.random()*10)
+
+    let palpite = Number(prompt("Chute um número de 0 a 10"))
+ 
+    console.log(aleatorio);
+    
+
+    if (palpite === aleatorio){
+      console.log("Parabéns, você acertou!")
+    }else{
+      console.log("Ahh, não foi dessa vez!")
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Exercício 1</h1>
+    <button onClick={adivinhar}>Adivinhar</button>
     </>
   )
 }
